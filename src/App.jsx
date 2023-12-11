fbf1a4ed71f672308704ded4f23094ec64e2d816
shivamshaw07 <shivamshaw9005@gmail.com>
2023-12-12 01:25:43 +0530

    done navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import {  Routes,Route} from "react-router-dom";
 
 function App() {
 
   return (
     <div className='w-[100vw] h-auto box-border'>
       <Navbar/>
+        
     </div>
   )
 }
