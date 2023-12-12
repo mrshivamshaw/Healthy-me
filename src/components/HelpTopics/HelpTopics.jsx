85c2b64cd2ae4f19a9c06ec740fee8044ffb1527
shivamshaw07 <shivamshaw9005@gmail.com>
2023-12-13 01:49:04 +0530

    changed bg

--- src/components/HelpTopics/HelpTopics.jsx
+++ src/components/HelpTopics/HelpTopics.jsx
@@ -3,7 +3,7 @@ import Cards from './Cards'
 
 const HelpTopics = () => {
   return (
-    <div className='w-[100vw] h-auto'>
+    <div className='w-[100vw] h-auto bg-white'>
         <div className='w-[85vw] mx-auto flex flex-col font-satoshi justify-start items-start gap-5 '>
             <h2 className='text-[24px] font-[900] tracking-widest text-transparent' style={{background: "linear-gradient(96.14deg, #3A8EF6 -10.84%, #6F3AFA 196.74%)",backgroundClip:'text'}}>HELP TOPICS</h2>
             <h1 className='text-[40px] font-[700] '>Enhance Your Lifestyle</h1>
