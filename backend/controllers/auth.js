da2f17d09f0bea30e3e1f81ae5fca4e9fe3af011
shivamshaw07 <shivamshaw9005@gmail.com>
2023-12-15 00:29:55 +0530

    log in done

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,6 +1,8 @@
-import mongoose from "mongoose";
 import user from "../models/user.js";
 import bcrypt from 'bcrypt'
+import jwt  from "jsonwebtoken";
+import {config as configDotenv } from "dotenv";
+configDotenv()
 
 export const signin = async (req,res) =>{
     try {
@@ -61,5 +63,67 @@ export const signin = async (req,res) =>{
             sucess: false,
             message : "something went wrong while registering user",
         })
+    }
+}
+
+export const login = async(req,res) =>{
+    try {
+        const {email,password} = req.body;
+        if(!email || !password){
+            return res.status(404).json({
+                success : false,
+                message : "All fields are required"
+            })
+        }
+        var userData = await user.findOne({email:email})
+
+        if(!userData){
+            return res.status(404).json(
+                {
+                    success:false,
+                    message : "User not found please singin"
+                }
+            )
+        }
+        const payload = {
+            email:userData.email,
+            id:userData._id
+        }
+        if(await bcrypt.compare(password,userData.password)){
+            const token = jwt.sign(payload,process.env.SECRET_KEY,{expiresIn:"2h"})
+            userData = userData.toObject()
+            userData.token = token
+            userData.password = undefined
+
+            const options = {
+                httpOnly:true,
+                expires: new Date(Date.now() + 30*24*60*60*1000)
+            }
+
+            res.cookie("token",token,options).status(200).json({
+                succes:true,
+                token:token,
+                message:"user successfully logged in",
+                userData
+            })
+        }
+
+        else{
+            return res.status(400).json(
+                {
+                    success:false,
+                    message : "Incorrect password"
+                }
+            )
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(404).json(
+            {
+                success:false,
+                message : "Error while logging user"
+            }
+        )
+
     }
 }
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -2,8 +2,10 @@ import express from 'express'
 
 const router = express.Router()
 
-import { signin } from '../controllers/auth.js'
+import { login, signin } from '../controllers/auth.js'
 
 router.post('/signin',signin)
+router.get('/login',login)
+
 
 export default router
