c8f9ccdc191dcadab95082ede64bda3051f46ed7
Bikash Agarwala <bikash.agarwala.01@gmail.com>
2024-01-04 17:34:38 +0530

    Some additions in challange section

--- frontend/src/components/Challenges/MyChallange/ChallangesOverview.jsx
+++ frontend/src/components/Challenges/MyChallange/ChallangesOverview.jsx
@@ -15,10 +15,12 @@ function ChallangesOverview() {
         </div>
 
         {/* Bottom Part */}
+        <div>
         <div className='bg-white flex h-20 w-full rounded-3xl fixed bottom-10 justify-around items-center'>
         <IoSearch className='text-[#8565F1] text-2xl'/>
             <button className='flex items-center  justify-center h-16 w-16 rounded-full bg-gradient-to-b from-blue-500 via-indigo-600 to-violet-600 shadow ' ><FaPlus className='text-white text-3xl text-center place-self-center'/></button>
             <IoReorderTwoSharp className='text-[#8565F1] text-2xl'/>
+        </div>
         </div>
     </div>
     </div>
