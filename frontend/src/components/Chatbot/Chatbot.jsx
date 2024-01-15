df899ad9f4082edb359681d95c541d05fd99eb87
Bikash Agarwala <bikash.agarwala.01@gmail.com>
2024-01-16 03:45:11 +0530

    Chatbot ui changes

--- frontend/src/components/Chatbot/Chatbot.jsx
+++ frontend/src/components/Chatbot/Chatbot.jsx
@@ -4,7 +4,8 @@ import { IoMdAttach } from "react-icons/io";
 import { MdOutlineEmojiEmotions } from "react-icons/md";
 import { BsFillSendFill } from "react-icons/bs";
 import TypingAnimation from './TypingAnimation';
-
+import send from "../../assets/send.png";
+import ChatbotIcon from "../../assets/ChatbotIcon.png";
 const API_KEY = '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'
 
 function Chatbot() {
@@ -51,7 +52,7 @@ function Chatbot() {
         <div className='h-[75vh] w-auto bg-[#F1F1FF] shadow-xl rounded-2xl drop-shadow-lg'>
             <div className='bg-white rounded-2xl'>
                 <div className='flex gap-10 border-b-2 p-3 px-5 py-10'>
-                    <div className='h-20 w-20 rounded-s-full bg-blue-400' />
+                    <img src={ChatbotIcon} className='h-20 w-20' />
                     <div className='self-center'>Healthbot</div>
                 </div>
                 <div className='p-5 px-12 text-[#5E5E5E]'>
@@ -64,7 +65,7 @@ function Chatbot() {
                 <div key={index}
                 className={`m-5 flex ${message.type === 'user'? 'justify-end' : 'justify-start'}`}
                 >
-                    <div className={`${message.type === 'user' ? 'bg-white rounded-br-none' : 'bg-blue-200 rounded-bl-none'} rounded-3xl p-4 `}>
+                    <div className={`${message.type === 'user' ? 'bg-white rounded-br-none' : 'bg-[#5D6EF7] rounded-bl-none'} rounded-3xl p-4 `}>
                         {message.message}
                     </div>
                     
@@ -73,12 +74,12 @@ function Chatbot() {
             </div>
             {isLoading && 
                 <div key={chatLog.length} className='flex justify-start'>
-                    <div className='bg-blue-200 rounded-3xl rounded-bl-none'>
+                    <div className='bg-[#5D6EF7] rounded-3xl rounded-bl-none'>
                         <TypingAnimation />
                     </div>    
                 </div>
             }
-            <form onSubmit={handleSubmit} className='fixed bottom-0 h-12 w-[80%] rounded-xl m-10 p-3 bg-white text-xl'>
+            <form onSubmit={handleSubmit} className='fixed bottom-0 h-12 w-[80%] rounded-xl m-10 p-3 bg-white text-xl flex'>
                 <input className='w-[70%] h-full outline-none rounded-3xl' type="text"
                  value={inputValue}
                  onChange={(e) => setInputValue(e.target.value)} 
@@ -86,7 +87,7 @@ function Chatbot() {
                 />
                 <button className=' text-[#A3A3A3] px-2 scale-105'><IoMdAttach /></button>
                 <button className='text-[#A3A3A3] px-2 scale-105'><MdOutlineEmojiEmotions /></button>
-                <button type='submit'  className='text-[#5D6EF7] px-5 scale-150 drop-shadow'><BsFillSendFill /></button>
+                <button type='submit'  className='text-[#5D6EF7] px-5 drop-shadow'><img src={send} alt="tele" className="h-[30px]" /></button>
             </form>
         </div>
     </div>
--- frontend/src/components/Chatbot/TypingAnimation.jsx
+++ frontend/src/components/Chatbot/TypingAnimation.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 function TypingAnimation() {
   return (
     <div className='flex items-center space-x-2'>
-        <div className='w-4 h-4 rounded-full bg-blue-200 animate-pulse delay-75' />
-        <div className='w-4 h-4 rounded-full bg-blue-200 animate-pulse delay-150' />
-        <div className='w-4 h-4 rounded-full bg-blue-200 animate-pulse delay-200' />
+        <div className='w-4 h-4 rounded-full bg-[#5D6EF7] animate-pulse delay-75' />
+        <div className='w-4 h-4 rounded-full bg-[#5D6EF7] animate-pulse delay-150' />
+        <div className='w-4 h-4 rounded-full bg-[#5D6EF7] animate-pulse delay-200' />
     </div>
   )
 }
