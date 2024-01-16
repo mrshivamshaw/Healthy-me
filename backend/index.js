6c47e80a5af129f073124e0e9d3069f349207756
shivamshaw07 <shivamshaw9005@gmail.com>
2024-01-16 20:30:19 +0530

    leaderboard

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,18 @@ import { config as configDotenv } from "dotenv";
 import dbConnect from "./config/database.js";
 import router from "./routes/route.js";
 import cors from 'cors'
-
+const app = express();
+app.use(cors())
+app.use(express.json());
 configDotenv();
 
 dbConnect();
-const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(express.json());
-app.use(cors())
+
 app.use('/api/v1', router);
 
-// Define the '/' route before starting the server
+// Define the '/' route before starting the server  
 app.get('/', (req, res) => {
     res.send("Hello jii");
 });
