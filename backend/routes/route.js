02b831c4a2376452d7c4b95c5ba740f2a442e8dc
shivamshaw07 <shivamshaw9005@gmail.com>
2024-02-10 23:52:57 +0530

    adding clinic model

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,37 +1,44 @@
-import express from 'express'
+import express from 'express';
+import { login, signin } from '../controllers/auth.js';
+import handelChallenge from '../controllers/challenge.js';
+import fetchUserChallenges from '../controllers/getUserChallenges.js';
+import { updateChallenge } from '../controllers/challengeComplete.js';
+import multer from 'multer';
+import user from '../models/user.js';
+import clinicController from '../controllers/clinic.js';
+import { checkAuth } from '../middllewares/auth.js';
 
-const router = express.Router()
+const router = express.Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-import { login, signin } from '../controllers/auth.js'
-import { checkAuth } from '../middllewares/auth.js'
-import handelChallenge from '../controllers/challenge.js'
-import fetchUserChallenges from '../controllers/getUserChallenges.js'
-import { updateChallenge } from '../controllers/challengeComplete.js'
-import user from '../models/user.js'
-router.post('/signin',signin),
-router.post('/login',login)
-router.post('/checkAuth',checkAuth,(req, res) => {
-    // This route is protected and will only be accessible if the token is valid
+router.post('/signin', signin);
+router.post('/login', login);
+
+router.post('/checkAuth', checkAuth, (req, res) => {
     res.status(200).json({ message: 'Access granted' });
-  });
-router.post('/createChallenge',handelChallenge)
-router.post('/challengeCompleted',updateChallenge)
-router.get('/userchallenge/:id',fetchUserChallenges)
-router.get('/getAllUser', async (req, res) => {
-  try {
-    const users = await user.find({}).exec();
-    const sortedData = users.sort((a, b) => b.totalPoint - a.totalPoint);
-    res.status(200).json({
-      success: true,
-      data: sortedData
-    });
-  } catch (err) {
-    res.status(404).json({
-      success: false,
-      message: err.message
-    });
-  }
 });
 
+router.post('/createChallenge', handelChallenge);
+router.post('/challengeCompleted', updateChallenge);
+router.get('/userchallenge/:id', fetchUserChallenges);
 
-export default router
+router.get('/getAllUser', async (req, res) => {
+    try {
+        const users = await user.find({}).exec();
+        const sortedData = users.sort((a, b) => b.totalPoint - a.totalPoint);
+        res.status(200).json({
+            success: true,
+            data: sortedData
+        });
+    } catch (err) {
+        res.status(404).json({
+            success: false,
+            message: err.message
+        });
+    }
+});
+
+router.post('/clinicDatails', upload.single('file'), clinicController);
+
+export default router;
